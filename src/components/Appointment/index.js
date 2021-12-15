eb7485bd199e069207e068c5416c5337f174ba2d
Gabriela Guimaraes <gabriela.g.ferreira91@gmail.com>
2021-12-15 00:47:21 -0500

    Make editing possible for each appointment

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -14,6 +14,7 @@ export default function Appointment(props) {
   const SAVING = "SAVING";
   const DELETING = "DELETING";
   const CONFIRM = "CONFIRM";
+  const EDIT = "EDIT";
   
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
@@ -43,6 +44,7 @@ export default function Appointment(props) {
           student={props.interview.student} 
           interviewer={props.interview.interviewer.name}
           onDelete={() => transition(CONFIRM)}
+          onEdit={() => transition(EDIT)}
         />}
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
       {mode === CREATE && 
@@ -59,6 +61,16 @@ export default function Appointment(props) {
           onCancel={back}
           onConfirm={cancel}
         />}
+      {mode === EDIT &&
+        <Form
+          student={props.interview.student}
+          interviewers={props.interviewers}
+          interviewer={props.interview.interviewer.id}
+          onSave={save}
+          onCancel={cancel}
+
+        />
+      }
     </article>
   );
 }
